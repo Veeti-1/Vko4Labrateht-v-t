d20ef6254fe4308b23796b3b242bed90f136c478
Veet-1 <veeti.salomaa155@gmail.com>
2025-09-28 14:48:24 +0300

    osio1 tehty

---
+++ osa1/app/src/main/java/javalabravko3/teht2.java
@@ -0,0 +1,21 @@
+
+package javalabravko3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class teht2 {
+     public static void main(String[] args) {
+
+        //tehtävä2
+        List<Integer> teht2 = new ArrayList <Integer>();
+        teht2.add(10);
+        teht2.add(20);
+        System.out.println(teht2);
+
+ 
+        
+        
+    }
+
+}
---
+++ osa1/app/src/main/java/javalabravko3/teht6.java
@@ -0,0 +1,24 @@
+package javalabravko3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class teht6 {
+      public static void main(String[] args) {
+       
+
+        //tehtävä 6
+        List <Integer> teht6 = new ArrayList<Integer>();
+        teht6.add(1);
+        teht6.add(2);
+        teht6.add(3);
+        for(Integer i : teht6){
+            System.out.println(i);
+        }
+        for (Integer i : teht6) {
+            System.out.println(i);
+        }
+        
+        
+    }
+}
